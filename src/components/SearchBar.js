f62ca61e8f669dcfc1b56b20e5d5702effd8a518
rex <rexmo@hku.hk>
2021-06-05 19:23:11 +0800

    fire search with enter

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Input, InputAdornment, IconButton } from '@material-ui/core'
 import Search from '@material-ui/icons/Search'
 
 const SearchBar = ({onChange, onSubmit}) => {
+    const onKeyPress = useCallback(evt => {
+        if (evt.charCode === 13) {
+            onSubmit()
+            evt.target.blur()
+        }
+    }, [onSubmit])
     return (<div>
-        <Input label="Search" onChange={onChange} fullWidth
+        <Input
+        label="Search"
+        onChange={onChange}
+        fullWidth
+        onKeyPress={onKeyPress}
         endAdornment={
             <InputAdornment position="end">
                 <IconButton
