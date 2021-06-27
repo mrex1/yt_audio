c399bd080cc5b0564316da17eaaaca20de2c45fa
mrex1 <42565086+mrex1@users.noreply.github.com>
2021-06-27 20:05:33 +0800

    seekable audio (#9)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ import {SuggestVideo} from '../types'
 export const zeroPad = (num: number, places: number) => String(num).padStart(places, '0')
 
 export const formatTime = (time: number) => {
+    time = Math.ceil(time)
     return `${zeroPad(Math.floor(time/60), 2)}:${zeroPad(time%60, 2)}`
 }
 
