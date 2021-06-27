mrex1/yt_audio

commit 72e9fa8392722067269ea4226ceb1a006422510e

Bug fix/player bugs (#8)
* round up current time

* reset time to zero when audio changed

src/components/Player.tsx
@@ -32,11 +32,12 @@ const Player = ({videoDetails, onVideoEnd, onVideoStart, autoplay, setAutoplay}:
             setCurVId(videoDetails.id)
             if (audioRef.current) {
                 audioRef.current.pause()
-                audioRef.current.currentTime = 0
+                setCurrentTime(0)
             }
             const audio = api.getAudio(videoDetails.id)
             audio.addEventListener('timeupdate', () => {
-                setCurrentTime(Math.round(audio.currentTime))
+                if (audio.paused) return
+                setCurrentTime(Math.ceil(audio.currentTime))
             })
             audio.addEventListener('ended', () => {
                 setPlaying(false)

commit 1b9b95906871d6f6c961b36a8af04fc0b7947c40

quick_fix/pausing bug caused by commit 72e9fa8392722067269ea4226ceb1a006422510e

src/components/Player.tsx
@@ -32,13 +32,13 @@ const Player = ({videoDetails, onVideoEnd, onVideoStart, autoplay, setAutoplay}:
             setCurVId(videoDetails.id)
             if (audioRef.current) {
                 audioRef.current.pause()
+                audioRef.current.ontimeupdate = null
                 setCurrentTime(0)
             }
             const audio = api.getAudio(videoDetails.id)
-            audio.addEventListener('timeupdate', () => {
-                if (audio.paused) return
+            audio.ontimeupdate = () => {
                 setCurrentTime(Math.ceil(audio.currentTime))
-            })
+            }
             audio.addEventListener('ended', () => {
                 setPlaying(false)
                 if (onVideoEnd) {