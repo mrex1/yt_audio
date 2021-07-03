2ed2bd5957bf29a5d31eb8cab060e3e086cdb10a
mrex1 <42565086+mrex1@users.noreply.github.com>
2021-07-03 14:00:02 +0800

    Enhancement/refactor audio (#12)
    * separate audio logic
    
    * separate css; new implementation for update audio

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { URL } from '../constants'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Slider, IconButton, Typography, Tooltip, CircularProgress } from '@material-ui/core'
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled'
 import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled'
 import { formatTime, durationToSeconds } from '../utils'
-import { api } from '../services'
+import { api, audioManager } from '../services'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow'
 import PauseIcon from '@material-ui/icons/Pause'
 import DownloadIcon from '@material-ui/icons/GetApp'
 import { Video } from 'ytsr'
 import { SuggestVideo } from '../types'
+import './Player.css'
 
 
 interface Props {
@@ -25,69 +25,59 @@ const Player = ({ videoDetails, onVideoEnd, onVideoStart, autoplay, setAutoplay 
     const [curVId, setCurVId] = useState<string | null>(null)
     const [loading, setLoading] = useState<boolean>(false)
     const [currentTime, setCurrentTime] = useState<number>(0)
-    const audioRef = useRef<HTMLAudioElement | null>(null)
 
-    const fetchAudio = useCallback(async (videoDetails: Video | SuggestVideo) => {
-        setCurVId(videoDetails.id)
-        if (audioRef.current) {
-            audioRef.current.pause()
-            audioRef.current.ontimeupdate = null
-            setCurrentTime(0)
-        }
-        const audio = api.getAudio(videoDetails.id)
-        if (!audio) return
-        audio.ontimeupdate = () => {
-            setCurrentTime(Math.ceil(audio.currentTime))
-        }
-        audio.addEventListener('ended', () => {
-            setPlaying(false)
-            if (onVideoEnd) {
-                onVideoEnd()
-            }
-        })
-        audio.addEventListener('pause', () => {
-            setPlaying(false)
-        })
-        audio.addEventListener('play', () => {
+    useEffect(() => {
+        audioManager.audio.ontimeupdate = () => setCurrentTime(Math.ceil(audioManager.audio.currentTime))
+        audioManager.audio.onpause = () => setPlaying(false)
+        audioManager.audio.onloadeddata = () => setLoading(false)
+        audioManager.audio.onloadstart = () => setLoading(true)
+    }, [])
+
+    useEffect(() => {
+        audioManager.audio.onplay = () => {
             setPlaying(true)
             if (onVideoStart) {
                 onVideoStart()
             }
-        })
-        audioRef.current = audio
-        if (autoplay) {
-            audio.play()
         }
-    }, [onVideoEnd, onVideoStart, autoplay])
+        audioManager.audio.onended = () => {
+            setPlaying(false)
+            if (onVideoEnd) {
+                onVideoEnd()
+            }
+        }
+    }, [onVideoStart, onVideoEnd])
+
+    const setupForNewVideo = useCallback((videoDetails: Video | SuggestVideo) => {
+        setCurVId(videoDetails.id)
+        audioManager.updateAudio(videoDetails.id)
+        setCurrentTime(0)
+        if (autoplay) {
+            audioManager.audio.play().catch(console.log)
+        }
+    }, [autoplay])
 
     useEffect(() => {
-        if (!loading && curVId !== videoDetails.id) {
-            setLoading(true)
-            fetchAudio(videoDetails).then(() => setLoading(false))
+        if (curVId !== videoDetails.id) {
+            setupForNewVideo(videoDetails)
         }
-    }, [fetchAudio, loading, curVId, videoDetails])
+    }, [setupForNewVideo, curVId, videoDetails])
 
     const download = useCallback(() => {
-        const a = document.createElement('a')
-        a.href = `${URL}/download?vid=${videoDetails.id}`
-        a.target = '_blank'
+        const a = api.getAudioDownloadLink(videoDetails.id)
         a.click()
     }, [videoDetails])
 
     const playOrPause = useCallback(() => {
-        if (audioRef.current) {
-            if (playing) {
-                audioRef.current.pause()
-            } else {
-                audioRef.current.play()
-            }
+        if (playing) {
+            audioManager.audio.pause()
+        } else {
+            audioManager.audio.play()
         }
     }, [playing])
 
     const onSliderChange = useCallback((_, val) => {
-        if (audioRef.current) {
-            audioRef.current.currentTime = val
-        }
+        audioManager.audio.currentTime = val
     }, [])
 
     const handleAutoplayBtn = useCallback(() => {
@@ -95,13 +85,13 @@ const Player = ({ videoDetails, onVideoEnd, onVideoStart, autoplay, setAutoplay 
     }, [autoplay, setAutoplay])
 
     return (
-        <div style={{ width: '100%', background: '#000', color: '#fff', display: 'flex', flexDirection: 'column', boxShadow: '0px 0px 4px rgba(0,0,0,0.5)' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <div className='player-container'>
+            <div className='player-progress-container'>
                 <span style={{ margin: 5, marginRight: 15 }}>{formatTime(currentTime)}</span>
                 <Slider color='secondary' onChange={onSliderChange} max={durationToSeconds(videoDetails.duration)} min={0} value={currentTime} />
                 <span style={{ margin: 5, marginLeft: 15 }}>{videoDetails.duration}</span>
             </div>
-            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <div className='player-bottom-container'>
                 {loading ? <div style={{ padding: 12 }}><CircularProgress color='secondary' size={22} /></div> :
                     <IconButton onClick={playOrPause} color='secondary'>
                         {playing ? <PauseIcon /> : <PlayArrowIcon />}
@@ -114,7 +104,7 @@ const Player = ({ videoDetails, onVideoEnd, onVideoStart, autoplay, setAutoplay 
                         {autoplay ? <PauseCircleFilledIcon /> : <PlayCircleFilledIcon />}
                     </IconButton>
                 </Tooltip>
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: 5, overflow: "hidden", textOverflow: "ellipsis" }}>
+                <div className='player-details-container'>
                     <Typography variant="subtitle1" noWrap>{videoDetails.title}</Typography>
                     <Typography variant="subtitle2" style={{ color: 'silver' }} noWrap>{videoDetails.author?.name}</Typography>
                 </div>
