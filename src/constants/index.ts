b0c1f7e76101ee2f07ed4b8a4f97d3e882a00831
rex <rexmo@hku.hk>
2021-07-21 09:35:54 +0800

    update api base url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import {createMuiTheme} from '@material-ui/core'
 
-export const URL = 'https://zb3ad4304-gtw.qovery.io'
+export const URL = 'https://zc374b33a-zb3ad4304-gtw.qovery.io'
 
 export const theme = createMuiTheme({
     palette: {
